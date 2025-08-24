0146978ccc37ceaea2ebcee24b776030a016ab9c
yeojhe <jhenderson2520@gmail.com>
2025-08-24 13:17:24 +0100

    suppress warnings (+non-user code) from clang-tidy

--- cmake/clang_tidy.cmake
+++ cmake/clang_tidy.cmake
@@ -1,10 +1,16 @@
-
-if (ENABLE_CLANG_TIDY)
-  find_program(CLANG_TIDY_EXE NAMES clang-tidy)
-  if (CLANG_TIDY_EXE)
-    message(STATUS "clang-tidy found: ${CLANG_TIDY_EXE}")
-    set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY_EXE};-p=${CMAKE_BINARY_DIR}")
-  else()
-    message(STATUS "clang-tidy not found")
+# cmake/clang_tidy.cmake
+function(enable_tidy TARGET)
+  if (ENABLE_CLANG_TIDY)
+    find_program(CLANG_TIDY_EXE NAMES clang-tidy)
+    if (CLANG_TIDY_EXE)
+      set(tidy_args
+        "${CLANG_TIDY_EXE}"
+        "-p=${CMAKE_BINARY_DIR}"
+        "-header-filter=^(src|tests)/.*"
+        # uncomment next line to force errors only for selected checks:
+        # "-warnings-as-errors=bugprone-*,performance-*,modernize-*"
+      )
+      set_property(TARGET ${TARGET} PROPERTY CXX_CLANG_TIDY "${tidy_args}")
+    endif()
   endif()
-endif()
+endfunction()
